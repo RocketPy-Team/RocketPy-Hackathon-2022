fc98611a56354616437deeeb743dff082aa5c511
jkan1001 <jkankainen@tamu.edu>
2022-08-28 12:59:53 -0500

    fixed indentation of chute_radius_finder

--- rocketpy/Analysis.py
+++ rocketpy/Analysis.py
@@ -42,34 +42,34 @@ class Analysis:
             return test_flight.apogee
         
         
-        def chute_radius_finder():
-            from numpy import pi
-            desiredterminal = float(input('Enter desired landing velocity in m/s '))
-            mass = self.rocket.mass # find a way to make sure this calls correctly
-            self.calculateDensityProfile()
-            d = self.density(1)
-        
-            parachute_type = input("Enter parachute type (eg: 'toroidal' or 'custom') ")
-        
-            if parachute_type == 'flat':
-                dragcoeff = 0.8
-            elif parachute_type == 'toroidal':
-                dragcoeff = 2.2
-            elif parachute_type == 'spherical':
-                dragcoeff = 1.5
-            elif parachute_type == 'custom':
-                dragcoeff = float(input('Enter custom drag coefficient '))
-            else:
-                print('Not a valid chute type, enter custom drag coefficient?')
-                dragcoeff = float(input())
-        
-        
-            desiredCdS = (2*mass*9.81)/(d*(desiredterminal)**2)
-        
-            area = desiredCdS / dragcoeff
-        
-            radius = (area / pi)**(1/2)
-        
-            print("Estimated required radius: {:.6f} meters".format(radius))
-        
-        return Function(apogee, inputs="Mass (kg)", outputs="Estimated Apogee (m)")
+    def chute_radius_finder():
+        from numpy import pi
+        desiredterminal = float(input('Enter desired landing velocity in m/s '))
+        mass = self.rocket.mass # find a way to make sure this calls correctly
+        self.calculateDensityProfile()
+        d = self.density(1)
+    
+        parachute_type = input("Enter parachute type (eg: 'toroidal' or 'custom') ")
+    
+        if parachute_type == 'flat':
+            dragcoeff = 0.8
+        elif parachute_type == 'toroidal':
+            dragcoeff = 2.2
+        elif parachute_type == 'spherical':
+            dragcoeff = 1.5
+        elif parachute_type == 'custom':
+            dragcoeff = float(input('Enter custom drag coefficient '))
+        else:
+            print('Not a valid chute type, enter custom drag coefficient?')
+            dragcoeff = float(input())
+    
+    
+        desiredCdS = (2*mass*9.81)/(d*(desiredterminal)**2)
+    
+        area = desiredCdS / dragcoeff
+    
+        radius = (area / pi)**(1/2)
+    
+        print("Estimated required radius: {:.6f} meters".format(radius))
+    
+    return Function(apogee, inputs="Mass (kg)", outputs="Estimated Apogee (m)")
