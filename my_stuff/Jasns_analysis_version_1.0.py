c312953e8f384ff330708d013d48b70094c5b675
a6zero <104808299+a6zero@users.noreply.github.com>
2022-08-27 19:04:08 -0500

    Create Jasns_analysis_version_1.0.py

---
+++ my_stuff/Jasns_analysis_version_1.0.py
@@ -0,0 +1,76 @@
+from .Flight import Flight
+from .Function import Function
+from .Rocket import Rocket
+
+class Analysis:
+    def __init__(self, flight: Flight):
+        self.flight = flight
+        self.rocket = self.flight.Rocket
+        self.motor = self.rocket.Motor
+        self.env = self.flight.Environment
+        
+    def apogee_by_mass(self, flight: Flight):
+        """
+        Returns a RocketPy Function that, for a given Flight configuration,
+        estimates the apogee as a function of the dry mass of the rocket.
+            Parameters:
+                flight (Flight):    Flight object to provide mass-apogee analysis
+            Returns:
+                RocketPy Function that provides predicted apogee as a function of dry mass
+        """
+        # Create version of flight that has ambigious mass
+        def apogee(mass):
+            variable_rocket = Rocket(
+                motor = self.motor,
+                radius = self.rocket.radius,
+                mass = mass,
+                inertiaI = self.rocket.inertiaI,
+                inertiaZ = self.rocket.inertiaZ,
+                distanceRocketNozzle = self.rocket.distanceRocketNozzle,
+                distanceRocketPropellant = self.rocket.distanceRocketPropellant,
+                powerOffDrag = 'idk', # FIX !!!!!!!!!!
+                powerOnDrag = 'idk' # FIX !!!!!!!!!!
+            )
+
+            test_flight = Flight(
+                rocket=variable_rocket,
+                environment=self.env,
+                inclination=flight.inclination,
+                heading=flight.heading,
+                terminateOnApogee=True,
+            )
+
+            return test_flight.apogee
+        
+        
+        def chute_radius_finder():
+            from numpy import pi
+            desiredterminal = float(input('Enter desired landing velocity in m/s '))
+            mass = self.rocket.mass # find a way to make sure this calls correctly
+            self.calculateDensityProfile()
+            d = self.density(1)
+        
+            parachute_type = input("Enter parachute type (eg: 'toroidal' or 'custom') ")
+        
+            if parachute_type == 'flat':
+                dragcoeff = 0.8
+            elif parachute_type == 'toroidal':
+                dragcoeff = 2.2
+            elif parachute_type == 'spherical':
+                dragcoeff = 1.5
+            elif parachute_type == 'custom':
+                dragcoeff = float(input('Enter custom drag coefficient '))
+            else:
+                print('Not a valid chute type, enter custom drag coefficient?')
+                dragcoeff = float(input())
+        
+        
+            desiredCdS = (2*mass*9.81)/(d*(desiredterminal)**2)
+        
+            area = desiredCdS / dragcoeff
+        
+            radius = (area / pi)**(1/2)
+        
+            print("Estimated required radius: {:.6f} meters".format(radius))
+        
+        return Function(apogee, inputs="Mass (kg)", outputs="Estimated Apogee (m)")
